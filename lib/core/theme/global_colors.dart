8ed74bc23b31fdf361af99c65206e6bb79b7dfc7
logan <gengjiarong@xiaoice.com>
2024-06-05 08:57:33 +0800

    init

---
+++ lib/core/theme/global_colors.dart
@@ -0,0 +1,46 @@
+import 'dart:ui';
+
+class GlobalColors{
+  static const Color primary = Color(0xFF3C50E0);
+
+  static const Color darkBackgroundColor = Color(0xff1E1C2A);
+
+  static const Color darkAppBar = Color(0xFF1C2434);
+
+  static const Color secondary = Color(0xFF80CAEE);
+
+  static const Color text = Color(0xFF64748B);
+
+  static const Color background = Color(0xFFEFF4FB);
+
+  static const Color border = Color(0xFFE5E8EC);
+
+  static const Color darkText = Color(0xFFDEE4EE);
+
+  static const Color darkTextBody = Color(0xFF8A99AF);
+
+  static const Color darkBlackText = Color(0xFF1C2434);
+
+  static const Color normal = Color(0xFF606266);
+
+  static const Color info = Color(0xFF8A99AF);
+
+  static const Color success = Color(0xFF10B981);
+
+  static const Color warn = Color(0xFFF0950C);
+
+  static const Color danger = Color(0xFFFB5454);
+
+  static const Color gray = Color(0xFFEFF4FB);
+
+  static const Color dark = Color(0xFF1C2434);
+
+  static const Color sideBar = Color(0xFF1C2434);
+
+  static const Color lightGray = Color(0xFFF7F9FC);
+
+
+}
+
+
+
---
+++ lib/routes.dart
@@ -0,0 +1,57 @@
+
+import 'package:flareline_crm/pages/home/crm_home_page.dart';
+import 'package:flutter/material.dart';
+
+typedef PathWidgetBuilder = Widget Function(BuildContext, String?);
+
+final List<Map<String, Object>> MAIN_PAGES = [
+  {'routerPath': '/', 'widget': CrmHomePage()},
+];
+
+class RouteConfiguration {
+  static final GlobalKey<NavigatorState> navigatorKey =
+      new GlobalKey<NavigatorState>(debugLabel: 'Rex');
+
+  static BuildContext? get navigatorContext =>
+      navigatorKey.currentState?.context;
+
+  static Route<dynamic>? onGenerateRoute(
+    RouteSettings settings,
+  ) {
+    String path = settings.name!;
+
+    dynamic map =
+        MAIN_PAGES.firstWhere((element) => element['routerPath'] == path);
+
+    if (map == null) {
+      return null;
+    }
+    Widget targetPage = map['widget'];
+
+    builder(context, match) {
+      return targetPage;
+    }
+
+    return NoAnimationMaterialPageRoute<void>(
+      builder: (context) => builder(context, null),
+      settings: settings,
+    );
+  }
+}
+
+class NoAnimationMaterialPageRoute<T> extends MaterialPageRoute<T> {
+  NoAnimationMaterialPageRoute({
+    required super.builder,
+    super.settings,
+  });
+
+  @override
+  Widget buildTransitions(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    return child;
+  }
+}
