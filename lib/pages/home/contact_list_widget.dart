a8bb885948ce0946a132e2d5a3f92c25d3fe5794
logan <gengjiarong@xiaoice.com>
2024-06-12 19:06:02 +0800

    update table

--- lib/pages/home/contact_list_widget.dart
+++ lib/pages/home/contact_list_widget.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flareline_uikit/components/forms/search_widget.dart';
 import 'package:flareline_uikit/components/forms/select_widget.dart';
+import 'package:flareline_uikit/components/image/image_widget.dart';
 import 'package:flareline_uikit/components/tables/table_widget.dart';
 import 'package:flareline_uikit/entity/table_data_entity.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +12,14 @@ class ContactListWidget extends TableWidget<ContactListViewModel> {
   @override
   // TODO: implement showPaging
   bool get showPaging => false;
+
+  @override
+  // TODO: implement showCheckboxColumn
+  bool get showCheckboxColumn => true;
+
+  @override
+  // TODO: implement rowHeight
+  double get rowHeight => 80;
 
   @override
   String title(BuildContext context) {
@@ -41,6 +50,39 @@ class ContactListWidget extends TableWidget<ContactListViewModel> {
   }
 
   @override
+  Widget? customWidgetsBuilder(BuildContext context,
+      TableDataRowsTableDataRows columnData, ContactListViewModel viewModel) {
+    if ('firstName' == columnData.columnName) {
+      return Row(
+        children: [
+          ImageWidget(
+            imageUrl: columnData.data['avatar'],
+            width: 60,
+            height: 60,
+            isCircle: true,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Text(columnData.data['name'])
+        ],
+      );
+    }
+
+    if('status' == columnData.columnName){
+      return Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4),
+          color:  Color(0xFFF2F6FF)
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 8,vertical: 3),
+        child: Text(columnData.text??''),
+      );
+    }
+    return null;
+  }
+
+  @override
   Widget? actionWidgetsBuilder(BuildContext context,
       TableDataRowsTableDataRows columnData, ContactListViewModel viewModel) {
     return Row(
@@ -50,16 +92,16 @@ class ContactListWidget extends TableWidget<ContactListViewModel> {
           width: 40,
           height: 40,
           alignment: Alignment.center,
-          decoration: BoxDecoration(
-              border: Border.all(color: Colors.black,width: 1)
+          decoration:
+              BoxDecoration(border: Border.all(color: Colors.black, width: 1)),
+          child: const Icon(
+            Icons.more_horiz,
+            size: 20,
           ),
-          child: const Icon(Icons.more_horiz,size: 20,),
         )
       ],
     );
   }
-
-
 
   @override
   ContactListViewModel viewModelBuilder(BuildContext context) {
